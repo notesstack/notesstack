33ff68d4d7f47f946fc18c6b4dcb65f00ead4bd0
ANAND <anand@ubuntu.(none)>
2013-12-12 16:53:04 +0530

    Routes added

--- UserProvider.js
+++ UserProvider.js
@@ -32,7 +32,7 @@ UserProvider.prototype.signup = function(email, username, password, callback)	{
 				console.log(JSON.stringify(err));
 			}
 			else if(result == null){
-				var hashpassword = hash(password)
+				var hashpassword = hash(password);
 				var uid = uniqueId();
 				var newUser = new User({ email: email, username:username, hash: hashpassword, uid:uid});
 				newUser.save(function(err, result){
@@ -64,11 +64,99 @@ UserProvider.prototype.login = function(email, password, callback)	{
 		}
 		else	{
 			if(validate(result.hash, password))	{
-					callback({RESULT_CODE:'1', MESSAGE:'Login successful'});
+					callback({RESULT_CODE:'1', MESSAGE:'Login successful', DATA: result.uid});
 			}
 			else	{
 					callback({RESULT_CODE:'1', MESSAGE:'Incorrect password'});
 			}
+		}
+	});
+}
+
+UserProvider.prototype.socialLogin = function(uid, email, username, access_token, firstname, lastname, avatar_name, callback)	{
+	User.findOne({uid:uid}, function(error, result)	{
+		if(error){
+			res.json({RESULT_CODE:'-1', MESSAGE:'System error'});
+			console.log(JSON.stringify(error));
+		}
+		else if(result == null)	{
+			var hashpassword = hash(access_token);
+			var newUser = new User({uid:uid, email: email, username:username, hash: hashpassword, firstname: firstname, lastname: lastname,
+											avatar: avatar_name});
+			newUser.save(function(err){
+				if(err)
+				{
+					callback({RESULT_CODE:'-1', MESSAGE:'System error'});
+				}
+				else
+				{
+					callback({RESULT_CODE:'1', MESSAGE:'User registered'});
+				}
+			});
+		}
+		else
+		{
+			var hashpassword = hash(access_token);
+			result.email = email;
+			result.username = username;
+			result.hash = hashpassword;
+			result.firstname = firstname;
+			result.lastname = lastname;
+			result.avatar = avatar_name;
+			result.save(function(err)	{
+				if(err){
+					callback({RESULT_CODE:'-1', MESSAGE:'System error'});
+				}
+				else	{
+					callback({RESULT_CODE:'1', MESSAGE:'Login successful'});
+				}
+			});
+		}
+	});
+}
+
+UserProvider.prototype.updateProfile = function(uid, firstname, lastname, avatar_name, callback)	{
+	User.findOne({uid:uid}, function(error, result)	{
+		if(error){
+			callback({RESULT_CODE:'-1', MESSAGE:'System error'});
+		}
+		else if(result == null)	{
+			callback({RESULT_CODE:'-1', MESSAGE:'User does not exist'});
+		}
+		else
+		{
+			result.firstname = firstname;
+			result.lastname = lastname;
+			result.avatar = avatar_name;
+			result.save(function(err, sdata)	{
+				if(err){
+					callback({RESULT_CODE:'-1', MESSAGE:'System error'});
+				}
+				else	{
+					callback({RESULT_CODE:'1', MESSAGE:'Profile updated'});
+				}
+			});
+		}
+	});
+}
+
+UserProvider.prototype.getProfile = function(uid, callback)	{
+	User.findOne({uid:uid}, function(error, result)	{
+		if(error){
+			callback({RESULT_CODE:'-1', MESSAGE:'System error'});
+		}
+		else if(result == null)	{
+			callback({RESULT_CODE:'-1', MESSAGE:'User does not exist'});
+		}
+		else
+		{
+			var profiledata = {};
+			profiledata.username = result.username;
+			profiledata.firstname = result.firstname;
+			profiledata.lastname = result.lastname;
+			profiledata.avatar = result.avatar;
+			
+			callback(profiledata);
 		}
 	});
 }
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser({uploadDir:'./public/avatars'}));
 
 var userProvider = require('./UserProvider')
 var userProvider= new userProvider();
@@ -29,21 +30,42 @@ app.post('/login', function(req, res)	{
 	});
 });
 
-/*	
-app.post('/login/facebook', function(req, res)	{
-	var fbid = req.body.fbid;
+app.post('/login/social', function(req, res)	{
+	var uid = req.body.uid;
+	var email = req.body.email;
 	var username = req.body.username;
-	var password = req.body.password;
+	var access_token = req.body.access_token;
 	var firstname = req.body.firstname;
 	var lastname = req.body.lastname;
-	var avatar = req.body.avatar;
+	var tmp_path = req.files.avatar.path;
+	var avatar_path = tmp_path.split('/');
+	var avatar_name = avatar_path[2];
 
-	UpdateProvider.facebook(fbid, username, password, firstname, lastname, avatar, function(response)	{
+	userProvider.socialLogin(uid, email, username, access_token, firstname, lastname, avatar_name, function(response)	{
 		res.json(response);
 	});
 });
 
-*/
+app.post('/profile', function(req, res)	{
+	var uid = req.body.uid;
+	var firstname = req.body.firstname;
+	var lastname = req.body.lastname;
+	var tmp_path = req.files.avatar.path;
+	var avatar_path = tmp_path.split('/');
+	var avatar_name = avatar_path[2];
+
+	userProvider.updateProfile(uid, firstname, lastname, avatar_name, function(response)	{
+		res.json(response);
+	});
+});
+
+app.get('/profile/:uid', function(req, res)	{
+	var uid = req.params.uid;
+
+	userProvider.getProfile(uid, function(response)	{
+		res.json(response);
+	});
+});
 
 app.listen(3000);
 
